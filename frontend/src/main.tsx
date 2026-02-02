86bb859957f66285878ede5acc9044603d2f319f
Kushaal Kankane <kushaalkankane12345@gmail.com>
2026-02-02 16:30:23 +0000

    [Kushaal] Add Login and Signup frontend page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom"
 import App from './App.tsx'
+import LoginPage from './pages/LoginPage.tsx'
+import SignupPage from './pages/SignupPage.tsx'
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <>
+            <Route path='/' element={<App />} />
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/register' element={<SignupPage />} />
+        </>
+    )
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
