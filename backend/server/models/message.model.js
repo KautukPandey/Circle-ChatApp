64007c3dc4e743ba93658ad557e6158646657616
Kautuk <kautukpandey2004@gmail.com>
2026-02-02 22:34:53 +0530

    Add Chat and Message models for MongoDB schema

---
+++ backend/server/models/message.model.js
@@ -0,0 +1,33 @@
+import { Schema, model } from "mongoose";
+
+const messageSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    chat: {
+      type: Schema.Types.ObjectId,
+      ref: "Chat",
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["sent", "delivered", "seen"],
+      default: "sent",
+    },
+  },
+  { timestamps: true }
+);
+
+export default model("Message", messageSchema);
+    
