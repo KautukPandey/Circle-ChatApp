d171fd73f13d76c16944b2f477f235d7d82879e8
Kautuk <kautukpandey2004@gmail.com>
2026-02-02 21:58:34 +0530

    [Kautuk]Implement user authentication with registration and login functionality

--- backend/server/controllers/user.controller.js
+++ backend/server/controllers/user.controller.js
@@ -1,9 +1,8 @@
-import type { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import User from "../models/User.model.js";
+import User from "../models/user.model.js";
 
-export const register = async(req: Request,res: Response)=>{
+export const register = async(req,res)=>{
     try {
         const {username, email, password} = req.body;
 
@@ -15,12 +14,10 @@ export const register = async(req: Request,res: Response)=>{
             return res.status(400).json({message: "User already exists"})
         }
 
-        const hashedPassword = await bcrypt.hash(password,10);
-
         const user = await User.create({
             username,
             email,
-            password: hashedPassword
+            password
         })
 
         res.status(201).json({ message: "User registered" });
@@ -30,7 +27,7 @@ export const register = async(req: Request,res: Response)=>{
     }
 }
 
-export const login = async(req: Request,res: Response)=>{
+export const login = async(req,res)=>{
     try {
         const {email,password} = req.body;
 
@@ -38,18 +35,32 @@ export const login = async(req: Request,res: Response)=>{
         if (!user)
             return res.status(400).json({ message: "Invalid credentials" });
 
-        const isMatch = await bcrypt.compare(password,user.password)
-
+        const isMatch = await user.comparePassword(password)
         if(!isMatch)
             return res.status(400).json({ message: "Invalid credentials" });
         
         const token = jwt.sign(
-            { userId: user._id },
-            process.env.JWT_SECRET as string,
-            { expiresIn: "7d" }
-        );
+            {id: user._id},
+            process.env.JWT_SECRET,
+            {
+            expiresIn: '1h'
+            }
+        )
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 60 * 60 * 1000
+        })
 
-        res.json({ token });
+        return res.status(200).json({
+            message: 'Login successful',
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        })  
 
     } catch (error) {
         res.status(500).json({ message: "Server error" });
